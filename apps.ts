345566ddd8f9e39b30da8212e3ceb81f5dabb244
alinelmoura <127854712+alinelmoura@users.noreply.github.com>
2023-09-11 18:16:51 -0300

    Create apps.ts

---
+++ apps.ts
@@ -0,0 +1,107 @@
+/*
+Aline Nataly Lima de Moura- 164905
+Atividade 3
+Listar: users, rents e bikes
+metodo para autenticar a senha
+ps: usei o resto do codigo com base na correção da aula anterior
+*/
+
+import { Bike } from "./bike";
+import { Rent } from "./rent";
+import { User } from "./user";
+import crypto from 'crypto';
+
+export class App {
+    users: User[] = []
+    bikes: Bike[] = []
+    rents: Rent[] = []
+
+    findUser(email: string): User {
+        return this.users.find(user => user.email === email)
+    }
+
+    registerUser(user: User): string {
+        for (const rUser of this.users) {
+            if (rUser.email === user.email) {
+                throw new Error('Duplicate user.')
+            }
+        }
+        const newId = crypto.randomUUID()
+        user.id = newId
+        this.users.push(user)
+        return newId
+    }
+
+    registerBike(bike: Bike): string {
+        const newId = crypto.randomUUID()
+        bike.id = newId
+        this.bikes.push(bike)
+        return newId
+    }
+
+    removeUser(email: string): void {
+        const userIndex = this.users.findIndex(user => user.email === email)
+        if (userIndex !== -1) {
+            this.users.splice(userIndex, 1)
+            return
+        }
+        throw new Error('User does not exist.')
+    }
+    
+    rentBike(bikeId: string, userEmail: string, startDate: Date, endDate: Date): void {
+        const bike = this.bikes.find(bike => bike.id === bikeId)
+        if (!bike) {
+            throw new Error('Bike not found.')
+        }
+        const user = this.findUser(userEmail)
+        if (!user) {
+            throw new Error('User not found.')
+        }
+        const bikeRents = this.rents.filter(rent =>
+            rent.bike.id === bikeId && !rent.dateReturned
+        )
+        const newRent = Rent.create(bikeRents, bike, user, startDate, endDate)
+        this.rents.push(newRent)
+    }
+
+    returnBike(bikeId: string, userEmail: string) {
+        const today = new Date()
+        const rent = this.rents.find(rent => 
+            rent.bike.id === bikeId &&
+            rent.user.email === userEmail &&
+            rent.dateReturned === undefined &&
+            rent.dateFrom <= today
+        )
+        if (rent) {
+            rent.dateReturned = today
+            return
+        }
+        throw new Error('Rent not found.')
+    }
+
+    listaUser():User[] {
+        return this.users;
+    }
+
+    listaRent():Rent[] {
+        return this.rents;
+    }
+
+    listaBike():Bike[] {
+        return this.bikes;
+    }
+
+    autenticarUser(senhaInserida: string, emailInserido: string){
+        const usuario= this.users.find(user => user.email=== emailInserido)
+        
+        if(!usuario){
+            throw new Error('Usuario não encontrado.')
+        }
+        if(usuario.password===senhaInserida){
+                console.log('Autenticado!');
+        }
+        else throw new Error('Senha incorreta. Falha na autenticação.');
+    
+    }
+    
+    }
