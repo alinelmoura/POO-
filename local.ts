c1ac9fbb28c82a8d973d12063c8332b6b927b72a
alinelmoura <127854712+alinelmoura@users.noreply.github.com>
2023-09-18 16:30:06 -0300

    Create local.ts

---
+++ local.ts
@@ -0,0 +1,12 @@
+/*
+Aline Nataly Lima de Moura- 164905
+POO- 2023
+Atividade 6
+PS: Vou adicionar apenas os arquivos que fiz alguma alteração/inclui um novo*/
+
+export class Local{
+    constructor(
+        public latitude: number,
+        public longitude: number
+    ) {}
+}
