018e46e2a64656af0c13bf36d06843e9baa686bb
alinelmoura <127854712+alinelmoura@users.noreply.github.com>
2023-10-09 17:52:36 -0300

    Create rent-repo.ts

---
+++ rent-repo.ts
@@ -0,0 +1,13 @@
+import { Rent } from "../rent";
+
+export interface RentRepo {
+    add(rent: Rent): Promise<string>
+    findOpen(bikeId: string, userEmail: string): Promise<Rent>
+    update(id: string, rent: Rent): Promise<void>
+}
+
+export interface RentRepo {
+    add(rent: Rent): Promise<string>
+    findOpen(bikeId: string, userEmail: string): Promise<Rent>
+    update(id: string, rent: Rent): Promise<void>
+}
